6abb79d52135873b3354bf5d949a31e34ea0490d
hasibul islam <hasibul.islam180974@gmail.com>
2024-08-02 19:04:10 +0600

    to task done

--- day-04.js
+++ day-04.js
@@ -2,9 +2,16 @@
 // Tasks/Activities:
 // Activity 1: For Loop
 // • Task 1: Write a program to print numbers from 1 to 10 using a for loop.
+    for(i=1;i<=10;i++){
+        console.log(i)
+    }
 // • Task 2: Write a program to print the multiplication table of 5 using a for loop.
+for(i=1;i<=10;i++){
+    console.log(`5x${i}=${i*5}`);
+}
 // Activity 2: While Loop
 // • Task 3: Write a program to calculate the sum of numbers from 1 to 10 using a while loop.
+    
 // • Task 4: Write a program to print numbers from 10 to 1 using a while loop.
 // Activity 3: Do...While Loop
 // • Task 5: Write a program to print numbers from 1 to 5 using a do...while loop.
