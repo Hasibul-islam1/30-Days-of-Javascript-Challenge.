5677b5b756112826fc760f1705583741a90541fc
hasibul islam <hasibul.islam180974@gmail.com>
2024-08-02 19:18:23 +0600

    four task done

--- day-04.js
+++ day-04.js
@@ -11,8 +11,21 @@ for(i=1;i<=10;i++){
 }
 // Activity 2: While Loop
 // • Task 3: Write a program to calculate the sum of numbers from 1 to 10 using a while loop.
-    
+    let y =1;
+    let sum=0;
+    while (y<=10) {
+        console.log(sum,y)
+        sum=sum+y;
+        y++;
+    }
+    console.log(sum);
 // • Task 4: Write a program to print numbers from 10 to 1 using a while loop.
+    let z =10;
+    while (z>=1) {
+        console.log(z)
+        z--;
+       
+    }
 // Activity 3: Do...While Loop
 // • Task 5: Write a program to print numbers from 1 to 5 using a do...while loop.
 // • Task 6: Write a program to calculate the factorial of a number using a do...while loop.
