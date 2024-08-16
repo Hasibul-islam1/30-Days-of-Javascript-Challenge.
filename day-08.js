db52f9813ca49eeda10385a87d25587728888b74
hasibul islam <hasibul.islam180974@gmail.com>
2024-08-16 21:01:22 +0600

    add task

--- day-08.js
+++ day-08.js
@@ -15,12 +15,35 @@ amar sonar bangla ami tomay valo bashi
 console.log(AnthomSong);
 // Activity 2: Destructuring
 // • Task 3: Use array destructuring to extract the first and second elements from an array of numbers and log them to the console.
+let roll=[1,2,3,4,5,6,7,8];
+[a,b,...c]=[...roll]
+console.log(c);
 // • Task 4: Use object destructuring to extract the title and author from a book object and log them to the console.
+    let book={
+        BookName:"Nokhshi Khathar Mat",
+        author:"Joshim Uddin",
+        publish:1929
+    }
+     let{author,...p}=book;
+     console.log(p);
 // Activity 3: Spread and Rest Operators
 // • Task 5: Use the spread operator to create a new array that includes all elements of an existing array plus additional elements, and log the new array to the console.
+let num=[10,3,46,78,4457,...roll,568];
+num.forEach((x=>{
+    console.log(x);
+    
+}))
+console.log(num);
 // • Task 6: Use the rest operator in a function to accept an arbitrary number of arguments, sum them, and return the result.
+function sum(...ar) {
+    let add=ar.reduce((total,num)=>total+num,0);
+    console.log(add);
+    console.log(ar)
+}
+sum(...roll,9,10);
 // Activity 4: Default Parameters
 // • Task 7: Write a function that takes two parameters and returns their product, with the second parameter having a default value of 1. Log the result of calling this function with and
+
 // without the second parameter.
 // Activity 5: Enhanced Object Literals
 // • Task 8: Use enhanced object literals to create an object with methods and properties, and log the object to the console.
