f45d95709ac894a3925ef490bffc8811dd12432e
hasibul islam <hasibul.islam180974@gmail.com>
2024-08-20 21:09:27 +0600

    add task

--- day-10/day-10.js
+++ day-10/day-10.js
@@ -22,12 +22,22 @@
     })
 // Activity 3: Keyboard Events
 // • Task 5: Add a keydown event listener to an input field that logs the key pressed to the console.
+    let downBox=document.getElementById("information");
+    downBox.addEventListener("keydown",()=>{
+        document.getElementById("down").textContent=downBox.value;
+    })
 // • Task 6: Add a keyup event listener to an input field that displays the current value in a paragraph.
+   let Box= document.getElementById("info")
+   Box.addEventListener("keyup",()=>{
+        document.getElementById("press").textContent=Box.value;
+    })
 // Activity 4: Form Events
 // • Task 7: Add a submit event listener to a form that prevents the default submission and logs the form data to the console.
+
 // • Task 8: Add a change event listener to a select dropdown that displays the selected value in a paragraph.
 // Activity 5: Event Delegation
-// • Task 9: Add a click event listener to a list that logs the text content of the clicked list item using event delegation. • Task 10: Add an event listener to a parent element that listens for events from dynamically added child elements.
+// • Task 9: Add a click event listener to a list that logs the text content of the clicked list item using event delegation. 
+// • Task 10: Add an event listener to a parent element that listens for events from dynamically added child elements.
 // Feature Request:
 // 1. Click Event Script: Write a script that adds a click event listener to a button to change the text content of a paragraph.
 // 2. Mouse Events Script: Create a script that handles mouseover and mouseout events to change the background color of an element.
